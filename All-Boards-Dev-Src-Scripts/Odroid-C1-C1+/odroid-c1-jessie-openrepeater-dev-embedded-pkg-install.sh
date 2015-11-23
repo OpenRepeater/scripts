a23faff947eecb098d0f2820050b99b268143b1c
Richard Neese <r.neese@gmail.com>
2015-11-23 11:49:48 -0600

    fix scripts to ln paths

--- All-Boards-Dev-Src-Scripts/Odroid-C1-C1+/odroid-c1-jessie-openrepeater-dev-embedded-pkg-install.sh
+++ All-Boards-Dev-Src-Scripts/Odroid-C1-C1+/odroid-c1-jessie-openrepeater-dev-embedded-pkg-install.sh
@@ -692,6 +692,12 @@ ENV="ASYNC_AUDIO_NOTRIGGER=1"
 
 DELIM
 
+#FinalRequired Linking
+ln -s /var/www/openrepeater/sounds /usr/share/openrepeater/sounds
+rm /usr/share/svxlink/events.d/local
+ln -s /etc/openrepeater/svxlink/local-events.d/ /usr/share/svxlink/events.d/local
+ln -s /var/log/svxlink /var/www/openrepeater/log
+
 cat >> /etc/sudoers << DELIM
 #allow www-data to access amixer and service
 www-data   ALL=(ALL) NOPASSWD: /usr/local/bin/openrepeater_svxlink_restart, NOPASSWD: /usr/local/bin/openrepeater_svxlink_start, NOPASSWD: /usr/local/bin/openrepeater_svxlink_stop, NOPASSWD: /usr/bin/aplay, NOPASSWD: /usr/bin/arecord
