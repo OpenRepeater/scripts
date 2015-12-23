d016d5e28440a95e70e9ab493b86b7cb5b40ac83
Richard Neese <r.neese@gmail.com>
2015-12-23 08:30:14 -0600

    fix odroid scripts

--- RasPi-B+-RasPi-2/Raspi2-Raspbian-jessie-openrepeater-embedded-pkg-install.sh
+++ RasPi-B+-RasPi-2/Raspi2-Raspbian-jessie-openrepeater-embedded-pkg-install.sh
@@ -392,7 +392,7 @@ apt-get clean
 #####################################################
 #Working on sounds pkgs for future release of svxlink
 #####################################################
-wget https://github.com/kb3vgw/svxlink-sounds-en_US-heather/releases/download/15.11.2/svxlink-sounds-en_US-heather-16k-15.11.2.tar.bz2
+wget https://github.com/kb3vgw/svxlink-sounds-en_US-heather/releases/tag/15.11.2/svxlink-sounds-en_US-heather-16k-15.11.2.tar.bz2
 tar xjvf svxlink-sounds-en_US-heather-16k-15.11.2.tar.bz2
 mv en_US-heather-16k en_US
 mv en_US /usr/share/svxlink/sounds
