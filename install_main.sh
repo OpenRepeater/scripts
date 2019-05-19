08cbaca910978e3b2ee5ad7029492fdffe91b557
Aaron Crawford <alleghenycreative@users.noreply.github.com>
2019-05-19 06:13:19 -0400

    Change versioning from 2.1.0 (dev) to 2.1.0

--- install_main.sh
+++ install_main.sh
@@ -7,7 +7,7 @@
 ################################################################################
 # DEFINE VARIABLES (Scroll down for main script)
 ################################################################################
-ORP_VERSION="2.1.0 (dev)"
+ORP_VERSION="2.1.0"
 
 REQUIRED_OS_VER="9"
 REQUIRED_OS_NAME="Stretch"
