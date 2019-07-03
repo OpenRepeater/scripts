f264635c98e675e0d120a1f54ae83301f1d4e969
Aaron Crawford <info@alleghenycreative.com>
2019-07-03 00:42:11 -0400

    Update ORP version branch

--- functions/functions.sh
+++ functions/functions.sh
@@ -448,7 +448,7 @@ function install_orp_from_github {
 
 	rm -rf $WWW_PATH/$GUI_NAME/*
 	cd $WWW_PATH
-	git clone -b 2.1.x --single-branch https://github.com/OpenRepeater/openrepeater.git $WWW_PATH/$GUI_NAME
+	git clone -b 2.1.2 --single-branch https://github.com/OpenRepeater/openrepeater.git $WWW_PATH/$GUI_NAME
 
 	if [ $ORP_FILE_LOCATIONS = "dev" ]; then
 		#######################################################################
