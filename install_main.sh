be94dda0f907ee7a63e9bc482ba00c8fc8043e3d
Aaron Crawford <info@alleghenycreative.com>
2019-08-03 13:01:55 -0400

    Add include of new svxlink functions files...was missing

--- install_main.sh
+++ install_main.sh
@@ -45,6 +45,7 @@ source "${BASH_SOURCE%/*}/functions/menus.sh"
 
 # Include Main Functions File & RPI functions
 source "${BASH_SOURCE%/*}/functions/functions.sh"
+source "${BASH_SOURCE%/*}/functions/functions_svxlink.sh"
 source "${BASH_SOURCE%/*}/functions/functions_rpi.sh"
 source "${BASH_SOURCE%/*}/functions/functions_motd.sh"
 source "${BASH_SOURCE%/*}/functions/functions_ics.sh"
