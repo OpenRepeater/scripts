0de8ef1bb1e02f267034493b7bea11ffa78c26e7
Aaron Crawford <info@alleghenycreative.com>
2021-03-08 22:10:41 -0500

    Update versioning

--- install_main.sh
+++ install_main.sh
@@ -7,7 +7,7 @@
 ################################################################################
 # DEFINE VARIABLES (Scroll down for main script)
 ################################################################################
-ORP_VERSION="2.2.0 (dev)"
+ORP_VERSION="3.0.0 (dev)"
 
 REQUIRED_OS_VER="10"
 REQUIRED_OS_NAME="Buster"
