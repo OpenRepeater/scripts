5190477d9f663fb959952287fd8a5f3cfcf3be63
Rich <r.neese@gmail.com>
2022-07-29 15:17:44 -0400

    update new function

--- functions/functions_dummysnd.sh
+++ functions/functions_dummysnd.sh
@@ -5,4 +5,17 @@ cat >> /etc/modules << DELIM
 snd-dummy fake_buffer=0
 DELIM
     
+touch /etc/asound.conf
+ 
+cat >> /ect/asound.conf << DELIM
+pcm.card0 {
+    type hw
+    card 0
+}
+ctl.card0 {
+    type hw
+    card 0
+}
+DELIM
+   
 }
