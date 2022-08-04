e20be07649eab62d15ddbe21c794db14d605a2cd
Rich <r.neese@gmail.com>
2022-08-04 08:34:34 -0400

    updating /boot/config.txt
    disabling hdmi/dsi/csi/onboard audio

--- functions/functions_rpi.sh
+++ functions/functions_rpi.sh
@@ -12,10 +12,19 @@ function rpi_disables {
 	echo "--------------------------------------------------------------"
 	#/boot/config.txt
 	sed -i /boot/config.txt -e"s#dtparam=audio=on#\#dtparam=audio=on#"
+    sed -i /boot/config.txt -e"s#dtoverlay=vc4-kms-v3d#\#dtoverlay=vc4-kms-v3d#" 
+    sed -i /boot/config.txt -e"s#max_framebuffers=2#\#max_framebuffers=2#"
 
-	# Enable audio (loads snd_bcm2835)
-	# dtparam=audio=on
-	# /etc/modules
+    echo "--------------------------------------------------------------"
+    echo " Disable onboard Display/Camera Ports not used in OpenRepeater"
+    echo "--------------------------------------------------------------"
+    sed -i /boot/config.txt -e"s#camera_auto_detect=1#\#camera_auto_detect=1#"
+    sed -i /boot/config.txt -e"s#display_auto_detect=1#\#display_auto_detect=1#"
+    sed -i /boot/config.txt -e"s#disable_overscan=1#\#disable_overscan=1#"
+    
+    echo "--------------------------------------------------------------"
+    echo " Disable onboard Broadcom sound card not used in OpenRepeater"
+    echo "--------------------------------------------------------------"
 	sed -i /etc/modules -e"s#snd-bcm2835#\#snd-bcm2835#"
 
 }
