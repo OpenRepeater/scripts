483b8b92d2c57d3cdd2310c232c04fc15a5e8a7b
Rich <r.neese@gmail.com>
2022-09-03 09:52:18 -0400

    3.0.x scripts bullseye update

---
+++ functions/functions_dummysnd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+################################################################################
+# DEFINE DUMMY SOUND X86/AMD64 FUNCTIONS
+################################################################################
+function dummysnd_setup () {
+if [ "$system_arch" == "amd64" ] || [ "$system_arch" == "X86_64" ]; then
+    echo "--------------------------------------------------------------"
+    echo " Enable Dummy Sound X86/AMD64 Server"
+    echo "--------------------------------------------------------------"
+    modprobe snd-dummy
+
+	cat >> "/etc/modules" <<- DELIM
+		snd-dummy
+		DELIM
+    
+	touch /etc/asound.conf
+ 
+	cat >> "/etc/asound.conf" <<- DELIM
+		pcm.card0 {
+			type hw
+			card 0 
+		}
+		ctl.card0 {
+			type hw
+			card 0
+		}
+		DELIM
+fi        
+}
