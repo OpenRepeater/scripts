1ee7eb6ec1b9d00725c2224467a2ca64606ba847
Daniel Loranger <dloranger08@gmail.com>
2024-03-20 12:22:04 -0400

    Fixing path issue for cmake
    removing relative path ".." issue that prevents svxlink from building properly

--- functions/functions_svxlink.sh
+++ functions/functions_svxlink.sh
@@ -107,8 +107,8 @@ function install_svxlink_source () {
 	#mkdir build
 
 	#cd build
-	echo "make command: cmake -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON -DUSE_QT=no $Modules_Build_Cmake_switches .."
-	cmake -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON -DUSE_QT=no $Modules_Build_Cmake_switches ..
+	echo "make command: cmake -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON -DUSE_QT=no $Modules_Build_Cmake_switches"
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON -DUSE_QT=no $Modules_Build_Cmake_switches 
 	
 	make -j5
 	make doc
